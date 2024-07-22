1a1c6912285221ed6bf5f44e8a455f201fe34e50
Amitanshu Behura <144086900+amitanshu3@users.noreply.github.com>
2024-07-22 23:54:30 +0530

    updated FoodDisplay

--- Frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ Frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,7 +10,7 @@ const FoodDisplay = ({category}) => {
          <div className="food-display-list">
             {food_list.map((item,index)=>{
               if(category==='All'|| category===item.category.toLowerCase())  {
-                console.log(item.category)
+                
                 return <FoodItem key={index} id={item._id} name={item.name} price={item.price}    description={item.description} image={item.image} />
               }                   
                 
