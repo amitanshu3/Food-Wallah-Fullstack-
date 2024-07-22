e25e862e28d6106e1af23e52a527b84c3312e588
Amitanshu Behura <144086900+amitanshu3@users.noreply.github.com>
2024-07-22 23:57:59 +0530

    Update StoreContext.jsx

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const StoreContext = createContext(null);
 const storeContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-wallah-backend.onrender.com/";
 
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
