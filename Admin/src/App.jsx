bacf870d4da2b3ec4250c5a888d8a9bdd6e97f68
Amitanshu Behura <144086900+amitanshu3@users.noreply.github.com>
2024-09-17 10:19:23 +0530

    Update App.jsx

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const url = "https://foodwallahbackend-lgx2fw1l.b4a.run";
+  const url = "https://food-wallah-backend.onrender.com";
   return (
     <div>
       <ToastContainer />
